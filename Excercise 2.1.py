93d808ea86075c33836d7b425e4c1a9481128035
vincentiaputri72 <80144808+vincentiaputri72@users.noreply.github.com>
2021-03-11 16:46:19 +0700

    Add files via upload

---
+++ Excercise 2.1.py
@@ -0,0 +1,23 @@
+#tipe data Boolean
+print(True)
+print(type(True))
+
+#tipe data string
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+print(type("Ayo belajar Python"))
+
+#tipe data Integer
+print(20)
+print(type(20))
+
+#tipe data Float
+print(3.14)
+print(type(3.14))
+
+#tipe data complex
+print(5j)
+print(type(5j))
+
+#tipe data Hexadecimal
+print(9a)
