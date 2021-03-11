73520e55e2fb9c9ccc13163c64dfb51c787ae1f5
vincentiaputri72 <80144808+vincentiaputri72@users.noreply.github.com>
2021-03-11 17:08:27 +0700

    Add files via upload

---
+++ Excercise 2.3.py
@@ -0,0 +1,17 @@
+import math
+
+#menampilkan informasi program
+print("luas dan keliling lingkaran")
+
+#input nilai jari-jari
+r = floath(input("Masukkan nilai jari-jari; "))
+
+#menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#manghitung keliling lingkaran
+keliling_lingkaran = 2 * 3.14 * r
+
+#menampilkan hasil penghitungan ke layar
+print("luas lingkaran: ", luas_lingkaran)
+print("keliling lingkaran:", keliling_lingkaran)
